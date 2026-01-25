cc4230a0c212bf7509cb2e5dba8d8d3185ebf9f3
Kevin-Razafison <angelarknarem@gmail.com>
2026-01-25 11:46:49 +0300

    feat: add User entity and repository for auth system

---
+++ src/main/java/com/lumenmall/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lumenmall.backend.repository;
+
+import com.lumenmall.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
